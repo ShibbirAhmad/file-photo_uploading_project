23da642fc41fdfc2b23e1aefaf07dfc9daf2b699
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-02-08 23:28:05 +0600

    one file added in the inc folder

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer" class="bg-primary"> 
+	       <h2>www.shibbir it web world.com</h2>
+	</div>
+	
+	</div>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>this photo uploading system</title>
+	<link rel="stylesheet" href="inc/bootstrap.min.css" />
+	<script src="inc/bootstrap.min.js"> </script>
+	<script src="inc/jquery-3.3.1.min.js"> </script> 
+
+	
+	<link rel="stylesheet" href="inc/style.css"/>
+</head>
+<body>
+	<div class="container"> 
+	
+	<div class="header" class="bg-success"> 
+	   <h2 > this is photo/media uploading system </h2>
+	</div>
